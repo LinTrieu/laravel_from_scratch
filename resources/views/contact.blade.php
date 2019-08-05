333acef8bfd3e5b5f47f6e51f718686ee7e85c1c
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-08-05 18:11:23 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>Contact Form</h1>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+
+<h1>Contact Form</h1>
+
+<p>
+    <a href="/contact">Contact</a> us to learn more.
+</p>
+
+</body>
+</html>

aedb53c17ad354fd9ef8920a38b15b5e3d50dfa3
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-08-06 21:28:55 +0100

    Adds raw db JSON return in ProjectsController

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Project;
 use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
     public function index() {
-        return view('projects.index');
+
+        $projects = Project::all();
+        return $projects;
+
+        return view('projects.index', compound('projects'));
     }
 }
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,5 +6,12 @@
 
     <h1>My Projects</h1>
 
+    @foreach ($projects as $project)
+
+    <ul>
+        <li> {{ $project }}</li>
+    </ul>
+
+    @endsection
+
 @endsection
-git
